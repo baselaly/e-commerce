6bf269afb772f48af977fe79e8d43f44b97be79c
basel <basel.3ly@gmail.com>
2020-11-04 03:39:19 +0200

    carts table -model

---
+++ app/Models/Cart.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Http\Traits\UseUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Cart extends Model
+{
+    use HasFactory, UseUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'product_id', 'quantity'
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo('App\Models\Product');
+    }
+}
