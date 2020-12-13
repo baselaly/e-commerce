b865f291e65f8214a8565be3fc098daa75280251
baselaly <basel.3ly@gmail.com>
2020-12-13 04:31:03 +0200

    store like and some enhancments

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Like\ProductLikeRequest;
+use App\Http\Requests\Like\StoreLikeRequest;
 use App\Http\Resources\Response\ErrorResponse;
+use App\Http\Resources\Response\SuccessResponse;
 use App\Services\LikeService;
 
 class LikeController extends Controller
@@ -32,10 +34,25 @@ class LikeController extends Controller
         try {
             $like = $this->likeService->getSingleBy($likeData);
             $like = $this->likeService->delete($like);
-            return response()->json(['like' => $like], 200);
+            return response()->json(new SuccessResponse('unLiked Successfully'), 200);
         } catch (\Illuminate\Database\Eloquent\ModelNotFoundException$e) {
             $like = $this->likeService->create($likeData);
-            return response()->json(['like' => $like], 200);
+            return response()->json(new SuccessResponse('Liked Successfully'), 200);
+        } catch (\Throwable$e) {
+            return response()->json(new ErrorResponse($e->getMessage()), 500);
+        }
+    }
+
+    public function likeStore(StoreLikeRequest $request)
+    {
+        $likeData = ['user_id' => auth()->id(), 'likeable_id' => request('store_id'), 'likeable_type' => 'App\Models\Store'];
+        try {
+            $like = $this->likeService->getSingleBy($likeData);
+            $like = $this->likeService->delete($like);
+            return response()->json(new SuccessResponse('unLiked Successfully'), 200);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException$e) {
+            $like = $this->likeService->create($likeData);
+            return response()->json(new SuccessResponse('Liked Successfully'), 200);
         } catch (\Throwable$e) {
             return response()->json(new ErrorResponse($e->getMessage()), 500);
         }
--- routes/Apis/like.php
+++ routes/Apis/like.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'likes', 'middleware' => 'userAuth'], function () {
     Route::post('/product', [LikeController::class, 'likeProduct']);
+    Route::post('/store', [LikeController::class, 'likeStore']);
 });
